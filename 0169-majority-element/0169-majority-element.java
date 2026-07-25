bf72d0b74bc8cf82555118cf6833ece2583250a2
RISHIKESH <rv75749d@gmail.com>
2026-07-25 14:28:08 +0530

    Time: 17 ms (16.48%), Space: 52.6 MB (85.49%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        int majorityCount = nums.length / 2;
+
+        for (int num : nums) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+            
+    
+            if (counts.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}

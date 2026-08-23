7a75050cc47e0625bd28f7e38ad057c668b7cd69
RISHIKESH <rv75749d@gmail.com>
2026-08-23 23:30:25 +0530

    Time: 9 ms (29.93%), Space: 47.2 MB (26.28%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        // If the total number of '?' is odd, Alice always wins
+        if ((leftQ + rightQ) % 2 != 0) {
+            return true;
+        }
+
+        // Bob wins only if the difference in sum can be balanced out by the '?' difference
+        return 2 * (leftSum - rightSum) != 9 * (rightQ - leftQ);
+    }
+}
